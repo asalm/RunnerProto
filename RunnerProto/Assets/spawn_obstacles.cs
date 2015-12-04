c56ebc9c5302a6c99c1ec44e4f0d1c841bed259b
katrinxy1304 <katrinxy1304@gmail.com>
2015-12-04 10:44:42 +0100

    DeleteInstances
    Erstellte Instanzen vom Luafband werden nach einer gewissen Zeit
    gelöscht, um einen Objektoverflow zu vermeiden.
    Feinjustierung fehlt noch.

--- RunnerProto/Assets/spawn_obstacles.cs
+++ RunnerProto/Assets/spawn_obstacles.cs
@@ -1,40 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class spawn_obstacles : MonoBehaviour {
 
 
     public GameObject playerObject, platform;
-    int length = 8;
-    float x, distance, xPlatform;
+    List<GameObject> treadmill = new List<GameObject>();
+    int counter = 0;
+    int length = 8, maxDistance = 5;
+    //maxDistance ist die maximale Distanz die zurückgelegt werden darf, bis eine neue Platform erstellt wird
+    float currentXPosPlayer, distance, xPosPlatform;
+    float absoluteX;
 	// Use this for initialization
 	void Start () {
-        x = playerObject.transform.position.x;
-        xPlatform = platform.transform.position.x - length;
+
+        StartCoroutine(DeleteIntances());
+        currentXPosPlayer = playerObject.transform.position.x;
+        absoluteX = currentXPosPlayer;
+        xPosPlatform = platform.transform.position.x - length;
+
+        treadmill.Add(platform);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        distance = getDistance();
+        distance = getDistance(); //zurückgelegte Strecke vom Player
         if (distance < 0)
             distance *= -1;
-        if (distance >= 5)
+        if (distance >= maxDistance)
         {
-            x = playerObject.transform.position.x;
-            GameObject platformInstance = Instantiate(platform);
-            platformInstance.transform.position = new Vector3(xPlatform, platform.transform.position.y, platform.transform.position.z);
-            xPlatform -= length;
-            Debug.Log("instantiiert");
+            currentXPosPlayer = playerObject.transform.position.x;
+
+            GameObject platformInstance = (GameObject)Instantiate(platform); // Instanz des Laufbands wird erstellt
+            treadmill.Add(platformInstance); //neue Instanz wird in die Liste hinzugefügt
+
+            platformInstance.transform.position = new Vector3(xPosPlatform, platform.transform.position.y, platform.transform.position.z);
+            //oben: die erstellte Instanz wird hier neu positioniert
+
+            xPosPlatform -= length; //neue Position um die instantiierte Platform zu positionieren
+            Debug.Log(treadmill.Count);
         }
 
 	}
 
     public float getDistance() // errechnet die aktuelle zurückgelegt Distanz eines Gameobjects, was vom Nutzer festgelegt wird
-    {
-        if (x <= playerObject.transform.position.x)
-            return playerObject.transform.position.x - x;
+    {//je nachdem ob er sich rückwärts oder vorwärts bewegt, soll ein positiver Wert zurückgegeben werden, daher die Fallunterscheidung
+        if (currentXPosPlayer <= playerObject.transform.position.x)
+            return playerObject.transform.position.x - currentXPosPlayer;
         else
-            return x - playerObject.transform.position.x;
+            return currentXPosPlayer - playerObject.transform.position.x;
+    }
+
+
+
+    IEnumerator DeleteIntances()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            Destroy(treadmill[counter + 1]);
+            Debug.Log("Destroyed");
+            counter++;
+        }
     }
 }
